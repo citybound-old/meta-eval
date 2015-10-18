5cf5115b7f2134fe4a584bed028565c0f6edbb9d
Anselm Eickhoff <anselm.eickhoff@gmail.com>
2015-10-18 13:09:26 +0200

    better es6 compilation handling

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-import {transform} from "babel-core";
-import babelParser from "babel-core/lib/helpers/parse.js";
+"use strict";
 
-export default function metaEval (source, environment, alias, filename, sourceUrlBase, options) {
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+exports["default"] = metaEval;
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var _babelCore = require("babel-core");
+
+var _babelCoreLibHelpersParseJs = require("babel-core/lib/helpers/parse.js");
+
+var _babelCoreLibHelpersParseJs2 = _interopRequireDefault(_babelCoreLibHelpersParseJs);
+
+function metaEval(source, environment, alias, filename, sourceUrlBase, options) {
 
 	filename = filename || alias;
 	options = options || {};
 
 	if (options.transpile) {
 		try {
-			source = transform(source, {
+			source = (0, _babelCore.transform)(source, {
 				blacklist: ["regenerator", "es6.tailCall"],
 				loose: ["es6.forOf"],
 				optional: ["es7.classProperties"],
@@ -25,53 +37,44 @@ export default function metaEval (source, environment, alias, filename, sourceUr
 	alias = alias || "anonymousMetaProgram" + createKuid();
 	source = source + "\n//# sourceURL=" + sourceUrlBase + filename;
 
-	let executable = Object.create(Function.prototype);
-	let wrapperSource =
-`   // this function evaluates ${alias}
+	var executable = Object.create(Function.prototype);
+	var wrapperSource = "   // this function evaluates " + alias + "\n\n\t// catch syntax errors early\n\ttry {__parse(source);}\n\tcatch(e) {\n\t\tif (e instanceof SyntaxError) {\n\t\t\t__logSyntaxError(source, e);\n\t\t\treturn;\n\t\t} else throw e;\n\t}\n\n\teval(source);\n\t";
 
-	// catch syntax errors early
-	try {__parse(source);}
-	catch(e) {
-		if (e instanceof SyntaxError) {
-			__logSyntaxError(source, e);
-			return;
-		} else throw e;
-	}
-
-	eval(source);
-	`;
-
-	if (options.wrapperFileListed) wrapperSource += `//# sourceURL=${sourceUrlBase + "metaEval/" + alias}`;
+	if (options.wrapperFileListed) wrapperSource += "//# sourceURL=" + (sourceUrlBase + "metaEval/" + alias);
 
 	environment.__logSyntaxError = logSyntaxError;
-	environment.__parse = babelParser;
+	environment.__parse = _babelCoreLibHelpersParseJs2["default"];
 
-	let wrapperParameters = ["source"].concat(Object.keys(environment)).concat([wrapperSource]);
-	let wrapperFunction = Function.prototype.constructor.apply(executable, wrapperParameters);
+	var wrapperParameters = ["source"].concat(Object.keys(environment)).concat([wrapperSource]);
+	var wrapperFunction = Function.prototype.constructor.apply(executable, wrapperParameters);
 
-	let environmentValues = Object.keys(environment).map(function (k) {return environment[k];});
-	let wrapperArguments = [source].concat(environmentValues);
+	var environmentValues = Object.keys(environment).map(function (k) {
+		return environment[k];
+	});
+	var wrapperArguments = [source].concat(environmentValues);
 
 	wrapperFunction.apply(executable, wrapperArguments);
 
 	return environment;
-};
+}
 
-function logSyntaxError (source, e) {
+;
+
+function logSyntaxError(source, e) {
 	if (e.loc) {
-		console.error(
-			source.split(/\n/).slice(0, e.loc.line).map((l, i) => i + ":\t" + l).join("\n") +
-			"\n" + (e.loc.line + "").replace(/./g, "!") + "!\t" + source.split(/\n/)[e.loc.line - 1].slice(0, e.loc.column)
-				.replace(/[^\t]/g, "-") + "^"
-		);
+		console.error(source.split(/\n/).slice(0, e.loc.line).map(function (l, i) {
+			return i + ":\t" + l;
+		}).join("\n") + "\n" + (e.loc.line + "").replace(/./g, "!") + "!\t" + source.split(/\n/)[e.loc.line - 1].slice(0, e.loc.column).replace(/[^\t]/g, "-") + "^");
 	}
 	console.error(e);
 }
 
 // kinda unique id
-function createKuid () {
-	return 'xxxxxxxx'.replace(/[xy]/g, function(c) {
-		var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+function createKuid() {
+	return 'xxxxxxxx'.replace(/[xy]/g, function (c) {
+		var r = Math.random() * 16 | 0,
+		    v = c === 'x' ? r : r & 0x3 | 0x8;
 		return v.toString(16);
 	});
 }
+module.exports = exports["default"];
